6f9295e4ba83747fc88cbbc0f3bde564d8ec09c9
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-16 13:25:57 -0300

    Login

---
+++ application/views/login/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+    <link rel="icon" type="image/png" href="<?php echo base_url('icon.ico'); ?>">
+    <link href="<?php echo base_url('assets/css/bootstrap.css"'); ?> rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/sb-admin.css?'); ?>" rel="stylesheet">   
+    <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.min.css'); ?>">
+  </head>
+
+  <body background="<?php echo base_url('assets/img/fundo_login.jpg'); ?>" >
+
+      
+          <div class="col-md-6 col-md-offset-3">
+              <div class="jumbotron">
+                  <h1>Aruba Server</h1>
+                  <p>Servidor de Rede e Internet. <br />
+                  O Acesso ao sistem é restrito. Por favor efetue login.
+                  </p><br />
+                  <p><a class="btn btn-primary btn-lg" role="button">Entrar no Sistema</a></p>
+              </div>
+          </div>
+   
+
+      
