d771c36821398bc7c86bfef44ef6580ce457c902
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-16 12:54:59 -0300

    Tradução
    Inserção da biblioteca pt-BR para formulario e instalação da mesma no
    arquivo de configuração

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -19,7 +19,7 @@
               <div class="panel-body">
                 <div class="list-group">
                   <a href="#" class="list-group-item">
-                    <span class="badge">Offline</span>
+                    <span class="badge">Online</span>
                     <i class="fa fa-globe"></i> &nbsp;Serviço da Internet
                   </a>
                   <a href="#" class="list-group-item">
