a2cf2cf69ae29dd7920237c4caa893fa191df3a1
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-18 12:29:05 -0300

    Configuração do Bando de Dados

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -11,15 +11,18 @@ $dados_menu = array(
     'pg_user_cadastrados' => 'dashboard/rel_usuarios',
     'pg_envia_cobranca' => 'dashboard/cobranca',);
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
         $this->load->helper('form');
         $this->load->library('form_validation');
     }
 
-    public function index() {
+    public function index()
+    {
         global $dados_menu;
         $dados_menu['titulo_interno'] = 'Página Inicial';
         $this->load->view('includes/reader', $dados_menu);
@@ -28,18 +31,29 @@ class Dashboard extends CI_Controller {
         $this->load->view('includes/footer');
     }
 
-    public function cad_user() {
+    public function cad_user()
+    {
         global $dados_menu;
         $dados_menu['titulo_interno'] = 'Incluir Usuário';
         $dados_menu['sub_titulo_interno'] = '** Cadastro de usuários que utilizam a rede.';
-
+        
         $this->load->view('includes/reader', $dados_menu);
         $this->load->view('includes/menu_navegacao', $dados_menu);
-        $this->load->view('cad_usuario');
+
+        $this->form_validation->set_rules('nomecompleto', 'Nome do Usuario', 'required|trim');
+        $this->form_validation->set_rules('');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('cad_usuario');
+        } else {
+            echo "cadastra no banco";
+        }
+
         $this->load->view('includes/footer');
     }
 
-    public function cad_dispositivos() {
+    public function cad_dispositivos()
+    {
         global $dados_menu;
         $dados_menu['titulo_interno'] = 'Incluir Dispositovo';
         $dados_menu['sub_titulo_interno'] = '** Cadastro de dispositivos que utilizarão a rede.';
@@ -50,7 +64,8 @@ class Dashboard extends CI_Controller {
         $this->load->view('includes/footer');
     }
 
-    public function enviar_msg() {
+    public function enviar_msg()
+    {
         global $dados_menu;
         $dados_menu['titulo_interno'] = 'Enviar Mensagem';
         $dados_menu['sub_titulo_interno'] = '** Envie mensagem entre usuarios da rede.';
@@ -61,33 +76,34 @@ class Dashboard extends CI_Controller {
         $this->load->view('includes/footer');
     }
 
-    public function rel_usuarios() {
+    public function rel_usuarios()
+    {
         global $dados_menu;
-        
+
         $this->load->view('includes/reader', $dados_menu);
         $this->load->view('includes/menu_navegacao', $dados_menu);
         $this->load->view('enviar_msg');
         $this->load->view('includes/footer');
     }
 
-    public function cobranca() {   
+    public function cobranca()
+    {
         global $dados_menu;
         $dados_menu['titulo_interno'] = 'Enviar Cobrança';
         $dados_menu['sub_titulo_interno'] = '** Envie cobrança para os clientes da rede';
-        
+
         $this->form_validation->set_rules('valor', 'Valor cobrado', 'required|numeric');
 
         $this->load->view('includes/reader', $dados_menu);
         $this->load->view('includes/menu_navegacao', $dados_menu);
-        
-        /*Verifica se a validação passou*/
-        if($this->form_validation->run() == FALSE){
-        $this->load->view('cobranca');
-        }
-        else{
+
+        /* Verifica se a validação passou */
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('cobranca');
+        } else {
             echo passsei;
         }
-        
+
         $this->load->view('includes/footer');
     }
 
