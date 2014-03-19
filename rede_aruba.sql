dfee6a1a2dc51ed2d7a16521b4c1456abfd43326
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-19 17:58:58 -0300

    SQL Banco

--- rede_aruba.sql
+++ rede_aruba.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
--- Máquina: 127.0.0.1
--- Data de Criação: 19-Mar-2014 às 10:45
--- Versão do servidor: 5.6.14
--- versão do PHP: 5.5.6
+-- Host: 127.0.0.1
+-- Generation Time: 19-Mar-2014 às 21:58
+-- Versão do servidor: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de Dados: `rede_aruba`
+-- Database: `rede_aruba`
 --
 
 -- --------------------------------------------------------
@@ -27,20 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) NOT NULL,
-  `nomeusuario` varchar(20) NOT NULL,
+  `nomecompleto` varchar(45) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `apartamento` int(3) NOT NULL,
   `senha` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `nomeusuario`, `apartamento`, `senha`) VALUES
-(1, 'teste', 'teste', 405, '123');
+INSERT INTO `usuarios` (`nomecompleto`, `email`, `username`, `apartamento`, `senha`) VALUES
+('teste', '', 'teste', 405, '123');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
