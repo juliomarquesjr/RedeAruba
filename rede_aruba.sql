b12766ca1d7d5a418282123e0ead1fac79591d15
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-19 06:46:31 -0300

    SQL Banco

--- rede_aruba.sql
+++ rede_aruba.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 18-Mar-2014 às 21:23
--- Versão do servidor: 5.6.16
--- PHP Version: 5.5.9
+-- Máquina: 127.0.0.1
+-- Data de Criação: 19-Mar-2014 às 10:45
+-- Versão do servidor: 5.6.14
+-- versão do PHP: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `rede_aruba`
+-- Base de Dados: `rede_aruba`
 --
 
 -- --------------------------------------------------------
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   `nomeusuario` varchar(20) NOT NULL,
+  `apartamento` int(3) NOT NULL,
   `senha` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
@@ -38,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `nomeusuario`, `senha`) VALUES
-(1, 'teste', 'teste', '123');
+INSERT INTO `usuarios` (`id`, `nome`, `nomeusuario`, `apartamento`, `senha`) VALUES
+(1, 'teste', 'teste', 405, '123');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
