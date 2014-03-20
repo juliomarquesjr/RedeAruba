16b36fb150b2a92793c0c459e503f3900622f26d
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-20 14:02:25 -0300

    Atualização Login

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -9,58 +9,65 @@
         <title>Login</title>
         <link rel="icon" type="image/png" href="<?php echo base_url('icon.ico'); ?>">
         <link href="<?php echo base_url('assets/css/bootstrap.css"'); ?>" rel="stylesheet">
-              <link href="<?php echo base_url('assets/css/sb-admin.css?'); ?>" rel="stylesheet">   
+        <link href="<?php echo base_url('assets/css/sb-admin.css?'); ?>" rel="stylesheet">   
         <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.min.css'); ?>">
-       
     </head>
 
     <body background="<?php echo base_url('assets/img/fundo_login.png'); ?>" >
 
-        <!-- Jumbotron inicial-->
-        <div class="col-md-6 col-md-offset-3">
+        
+        <div class="col-md-5 col-md-offset-2">
             <div class="jumbotron">
                 <h1><i class="fa fa-desktop"></i> &nbsp;Aruba Server</h1>
                 <p>Servidor de Rede e Internet. <br />
                     O Acesso ao sistem é restrito. Por favor efetue login.
                 </p><br />
                 <p><a class="btn btn-primary btn-lg" role="button" data-toggle="modal" data-target="#loginModal">Entrar no Sistema</a></p>
-            </div>
-            <?php echo validation_errors(
-                    "<div class=\"alert alert-danger alert-dismissable\" id=\"erros\">
-                        <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>
-                            <strong>Atenção!</strong> ", "</div>");?>
-            
+            </div>     
+
         </div>
+        <div class="col-md-3">
 
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">Acesso ao Sistema</h4>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    echo form_open('login/verifica', 'role="form"');
 
-        <!-- Janela de Login -->
-        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Acesso ao Sistema</h4>
-                    </div>
-                    <div class="modal-body">
-                        <?php
-                        echo form_open('login/verifica', 'role="form"');
-                        echo "<div class=\"form-group\"><label>Nome de Usuario</label>";
-                        echo form_input(array('name' => 'usuario', 'class' => 'form-control'), set_value('usuario'));
+                    if (form_error('usuario') == TRUE) {
+                        echo "<div class=\"form-group has-error\"><label>Nome de Usuario</label>";
+                        echo form_input(array('name' => 'usuario', 'class' => 'form-control', 'placeholder' => 'Nome de Usuário'), set_value('usuario'));
+                        echo form_error('usuario');
                         echo "</div>";
+                    } else {
+                        echo "<div class=\"form-group\"><label>Nome de Usuario</label>";
+                        echo form_input(array('name' => 'usuario', 'class' => 'form-control', 'placeholder' => 'Senha'), set_value('usuario'));
+                        echo "</div>";
+                    }
+
+                    if (form_error('senha') == TRUE) {
+                        echo "<div class=\"form-group has-error\"><label>Senha</label>";
+                        echo form_password(array('name' => 'senha', 'class' => 'form-control'), '');
+                        echo form_error('senha');
+                        echo "</div>";
+                    } else {
                         echo "<div class=\"form-group\"><label>Senha</label>";
                         echo form_password(array('name' => 'senha', 'class' => 'form-control'), '');
                         echo "</div>";
-                        echo "
-                    </div>
-                    <div class=\"modal-footer\">
-                        <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Sair</button>" .
-                                
-                        form_submit(array('name' => 'enviar', 'value' => 'Logar no Sistema', 'class' => 'btn btn-primary')) .
-                        form_close();
-                        "</div>";
-                        ?>
+                    }
 
-                    </div>
+                    echo "<div class=\"modal-footer\">" .
+                    form_submit(array('name' => 'enviar', 'value' => 'Entrar', 'class' => 'btn btn-primary')) .
+                    form_close();
+                    "</div>";
+                    ?>
                 </div>
             </div>
+        </div>
+
+
+
+
 
