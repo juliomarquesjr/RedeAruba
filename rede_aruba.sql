fab48e72c342d01016a19a3b254b9a8b21897fd6
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-23 23:09:27 -0300

    SQL Banco

--- rede_aruba.sql
+++ rede_aruba.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.0.9
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 22-Mar-2014 às 13:11
--- Versão do servidor: 5.6.16
--- PHP Version: 5.5.9
+-- Máquina: 127.0.0.1
+-- Data de Criação: 24-Mar-2014 às 03:09
+-- Versão do servidor: 5.6.14
+-- versão do PHP: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,377 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `rede_aruba`
+-- Base de Dados: `aula`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+--
+-- Base de Dados: `cdcol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cds`
+--
+
+CREATE TABLE IF NOT EXISTS `cds` (
+  `titel` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `interpret` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `jahr` int(11) DEFAULT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=7 ;
+
+--
+-- Extraindo dados da tabela `cds`
+--
+
+INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`) VALUES
+('Beauty', 'Ryuichi Sakamoto', 1990, 1),
+('Goodbye Country (Hello Nightclub)', 'Groove Armada', 2001, 4),
+('Glee', 'Bran Van 3000', 1997, 5);
+--
+-- Base de Dados: `login_mvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sessoes`
+--
+
+CREATE TABLE IF NOT EXISTS `sessoes` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `sessoes`
+--
+
+INSERT INTO `sessoes` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('b5f00f9dcfaa426affbb903a4ba3bbb1', '127.0.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2', 1324137813, ''),
+('f7e6f0df1292748e8a5e0122e818d52a', '0.0.0.0', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36', 1393323492, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `codUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  `ativo` tinyint(4) NOT NULL,
+  PRIMARY KEY (`codUsuario`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`codUsuario`, `login`, `senha`, `ativo`) VALUES
+(1, 'dicasdephp', '44ee2557fc61539f65c1acbc8a8551c6', 1);
+--
+-- Base de Dados: `mercado`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `operadores`
+--
+
+CREATE TABLE IF NOT EXISTS `operadores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(35) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+--
+-- Base de Dados: `phpmyadmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_bookmark`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_bookmark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dbase` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `label` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `query` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Bookmarks' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_column_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_column_info` (
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `column_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `comment` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `mimetype` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `transformation` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `transformation_options` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column information for phpMyAdmin' AUTO_INCREMENT=22 ;
+
+--
+-- Extraindo dados da tabela `pma_column_info`
+--
+
+INSERT INTO `pma_column_info` (`id`, `db_name`, `table_name`, `column_name`, `comment`, `mimetype`, `transformation`, `transformation_options`) VALUES
+(1, 'mercado', 'operadores', 'id', '', '', '_', ''),
+(2, 'mercado', 'operadores', 'nome', '', '', '_', ''),
+(3, 'aula', 'usuarios', 'id', '', '', '_', ''),
+(4, 'aula', 'usuarios', 'nome', '', '', '_', ''),
+(5, 'aula', 'usuarios', 'email', '', '', '_', ''),
+(7, 'rede_aruba', 'usuarios', 'telefone', '', '', '_', ''),
+(8, 'rede_aruba', 'usuarios', 'bloco', '', '', '_', ''),
+(9, 'rede_aruba', 'usuarios', 'id', '', '', '_', ''),
+(10, 'rede_aruba', 'mensagem', 'id', '', '', '_', ''),
+(11, 'rede_aruba', 'mensagem', 'usuario', '', '', '_', ''),
+(12, 'rede_aruba', 'mensagem', 'assunto', '', '', '_', ''),
+(13, 'rede_aruba', 'mensagem', 'msg', '', '', '_', ''),
+(14, 'rede_aruba', 'mensagem', 'nova', '', '', '_', 'S'),
+(15, 'rede_aruba', 'mensagem', 'removida', '', '', '_', ''),
+(16, 'rede_aruba', 'dispositivos', 'id', '', '', '_', ''),
+(17, 'rede_aruba', 'dispositivos', 'usuario', '', '', '_', ''),
+(18, 'rede_aruba', 'dispositivos', 'nomedispositivo', '', '', '_', ''),
+(19, 'rede_aruba', 'dispositivos', 'ip', '', '', '_', ''),
+(20, 'rede_aruba', 'dispositivos', 'mac', '', '', '_', ''),
+(21, 'rede_aruba', 'dispositivos', 'data_criacao', '', '', '_', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_designer_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_designer_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `v` tinyint(4) DEFAULT NULL,
+  `h` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for Designer';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_history`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_history` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sqlquery` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`,`db`,`table`,`timevalue`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='SQL history for phpMyAdmin' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_pdf_pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_pdf_pages` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `page_nr` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `page_descr` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY (`page_nr`),
+  KEY `db_name` (`db_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_recent`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_recent` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `tables` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
+
+--
+-- Extraindo dados da tabela `pma_recent`
+--
+
+INSERT INTO `pma_recent` (`username`, `tables`) VALUES
+('root', '[{"db":"rede_aruba","table":"dispositivos"},{"db":"rede_aruba","table":"usuarios"},{"db":"rede_aruba","table":"mensagem"},{"db":"aula","table":"usuarios"},{"db":"rede_interna","table":"users"},{"db":"mercado","table":"operadores"},{"db":"phpmyadmin","table":"pma_column_info"},{"db":"phpmyadmin","table":"pma_designer_coords"},{"db":"phpmyadmin","table":"pma_history"},{"db":"phpmyadmin","table":"pma_pdf_pages"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_relation`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_relation` (
+  `master_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`master_db`,`master_table`,`master_field`),
+  KEY `foreign_field` (`foreign_db`,`foreign_table`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Relation table';
+
+--
+-- Extraindo dados da tabela `pma_relation`
+--
+
+INSERT INTO `pma_relation` (`master_db`, `master_table`, `master_field`, `foreign_db`, `foreign_table`, `foreign_field`) VALUES
+('rede_aruba', 'mensagem', 'usuario', 'rede_aruba', 'usuarios', 'id');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `pdf_page_number` int(11) NOT NULL DEFAULT '0',
+  `x` float unsigned NOT NULL DEFAULT '0',
+  `y` float unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for phpMyAdmin PDF output';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_info` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `display_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table information for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_table_uiprefs`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_uiprefs` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `prefs` text COLLATE utf8_bin NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`,`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tables'' UI preferences';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_tracking`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_tracking` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `version` int(10) unsigned NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `schema_snapshot` text COLLATE utf8_bin NOT NULL,
+  `schema_sql` text COLLATE utf8_bin,
+  `data_sql` longtext COLLATE utf8_bin,
+  `tracking` set('UPDATE','REPLACE','INSERT','DELETE','TRUNCATE','CREATE DATABASE','ALTER DATABASE','DROP DATABASE','CREATE TABLE','ALTER TABLE','RENAME TABLE','DROP TABLE','CREATE INDEX','DROP INDEX','CREATE VIEW','ALTER VIEW','DROP VIEW') COLLATE utf8_bin DEFAULT NULL,
+  `tracking_active` int(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`db_name`,`table_name`,`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='Database changes tracking for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pma_userconfig`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_userconfig` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `config_data` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User preferences storage for phpMyAdmin';
+
+--
+-- Extraindo dados da tabela `pma_userconfig`
+--
+
+INSERT INTO `pma_userconfig` (`username`, `timevalue`, `config_data`) VALUES
+('root', '2013-12-26 13:46:53', '{"lang":"pt"}');
+--
+-- Base de Dados: `rede_aruba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `dispositivos`
+--
+
+CREATE TABLE IF NOT EXISTS `dispositivos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` int(11) NOT NULL,
+  `nomedispositivo` varchar(40) NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `mac` varchar(20) NOT NULL,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Extraindo dados da tabela `dispositivos`
+--
+
+INSERT INTO `dispositivos` (`id`, `usuario`, `nomedispositivo`, `ip`, `mac`, `data_criacao`) VALUES
+(1, 1, '12', '12', '123', '2014-03-23 21:23:06'),
+(2, 3, 'teste', '123', '123', '2014-03-23 21:26:03'),
+(3, 1, 'julio', '122', '1233', '2014-03-23 21:30:06'),
+(4, 1, 'Console Xbox', '1235', '12334', '2014-03-23 21:33:06'),
+(5, 5, 'Xbox 360', '123456', '1332478', '2014-03-23 23:14:03');
 
 -- --------------------------------------------------------
 
@@ -33,20 +402,18 @@ CREATE TABLE IF NOT EXISTS `mensagem` (
   `msg` text NOT NULL,
   `nova` varchar(1) DEFAULT 'S',
   `removida` varchar(1) DEFAULT 'N',
-  `dataenvio` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `mensagem`
 --
 
-INSERT INTO `mensagem` (`id`, `usuario`, `assunto`, `msg`, `nova`, `removida`, `dataenvio`) VALUES
-(1, 1, 'test', 'te', 'S', 'N', '2014-03-21 11:54:34'),
-(2, 1, 'Teste de Mensagem', 'Ola boa noite\r\nVenho através dessa merda mostrar a mensagem.', 'S', 'N', '2014-03-21 11:54:34'),
-(3, 3, 'teste', 'teste', 'S', 'N', '2014-03-21 11:54:34'),
-(4, 3, 'Ultimo Teste', 'Efetuando o ultimo teste, vamos para o login', 'S', 'N', '2014-03-21 11:54:34'),
-(5, 3, 'Teste de assunto', 'Teste de Mensagem', 'S', 'N', '2014-03-21 17:04:10');
+INSERT INTO `mensagem` (`id`, `usuario`, `assunto`, `msg`, `nova`, `removida`) VALUES
+(1, 1, 'test', 'te', 'S', 'N'),
+(2, 1, 'Teste de Mensagem', 'Ola boa noite\r\nVenho através dessa merda mostrar a mensagem.', 'S', 'N'),
+(3, 3, 'teste', 'teste', 'S', 'N'),
+(4, 3, 'Ultimo Teste', 'Efetuando o ultimo teste, vamos para o login', 'S', 'N');
 
 -- --------------------------------------------------------
 
@@ -64,7 +431,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `telefone` varchar(30) NOT NULL,
   `senha` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -73,7 +440,33 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 INSERT INTO `usuarios` (`id`, `nomecompleto`, `email`, `username`, `apartamento`, `bloco`, `telefone`, `senha`) VALUES
 (1, 'Julio Cesar Marques', 'juliomarquesjr@yahoo.com.br', 'juliomarquesjr', 405, 'B', '96200959', '81dc9bdb52d04dc20036dbd8313ed055'),
 (3, 'Ingrid Albuquerque', 'ingridejulio@hotmail.com', 'ingrid', 506, 'B', '96962525', '81dc9bdb52d04dc20036dbd8313ed055'),
-(4, 'João do Pulo', 'juliodopulo@gol.com', 'joaopulo', 809, 'A', '123', '81dc9bdb52d04dc20036dbd8313ed055');
+(4, 'João do Pulo', 'juliodopulo@gol.com', 'joaopulo', 809, 'A', '123', '81dc9bdb52d04dc20036dbd8313ed055'),
+(5, 'Roger', 'roger@bol.com.br', 'rogermarques', 307, 'B', '91915520', '81dc9bdb52d04dc20036dbd8313ed055');
+--
+-- Base de Dados: `test`
+--
+--
+-- Base de Dados: `webauth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user_pwd`
+--
+
+CREATE TABLE IF NOT EXISTS `user_pwd` (
+  `name` char(30) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  `pass` char(32) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Extraindo dados da tabela `user_pwd`
+--
+
+INSERT INTO `user_pwd` (`name`, `pass`) VALUES
+('xampp', 'wampp');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
