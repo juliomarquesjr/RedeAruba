4417471f0b06cc3d3d4f079890967a58e3dcbc11
juliomarquesjr <juliomarquesjr@yahoo.com.br>
2014-03-27 23:12:15 -0300

    Comentarios

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -1,34 +1,45 @@
 <?php
 
-class Dashboard_model extends CI_Model
-{
-    public function do_insert($dados = NULL, $tabela = NULL, $pagina = NULL)
-    {
-        if ($dados != NULL) {
-            $this->db->insert($tabela, $dados);
-            $this->session->set_flashdata('insereOK', TRUE);
-            redirect($pagina);
-        }
-    }
+class Dashboard_model extends CI_Model {
+	
+	public function do_insert($dados = NULL, $tabela = NULL, $pagina = NULL) {
+		if ($dados != NULL) {
+			$this -> db -> insert($tabela, $dados);
+			$this -> session -> set_flashdata('insereOK', TRUE);
+			redirect($pagina);
+		}
+	}
+	
+	/**
+	 * Função para buscar todos os valores de uma determinada tabela
+	 * Recebe por referencia o nome da tabela
+	 */
+	public function get_all($tabela) {
+		return $this -> db -> get($tabela) -> result_array();
+	}
 
-    public function get_all($tabela)
-    {
-        return $this->db->get($tabela)->result_array();
-    }
-	
-	public function get_dispositivos(){
+	/**
+	 * Função para buscar os dispositivos cadastrados no banco de dados.
+	 */
+	public function get_dispositivos() {
 		$sql = "SELECT d.nomedispositivo, d.ip, d.mac, u.nomecompleto FROM dispositivos d, usuarios u WHERE d.usuario = u.id";
-		return $this->db->query($sql)->result_array();
+		return $this -> db -> query($sql) -> result_array();
 	}
-	
-	public function get_caixaEntrada(){
+
+	/**
+	 * Função para buscar a caixa de entrada dos emails
+	 */
+	public function get_caixaEntrada() {
 		$sql = "SELECT d.nomedispositivo, d.ip, d.mac, u.nomecompleto FROM dispositivos d, usuarios u WHERE d.usuario = u.id";
-		return $this->db->query($sql)->result_array();
+		return $this -> db -> query($sql) -> result_array();
 	}
-    
-    public function getClientes($id){
-        
-        return $this->db->get_where('usuarios', array('id' => $id))->row_array();
-        
-    }
+
+	/*
+	 * Função para buscar os clientes do banco de dados.
+	 */
+	public function getClientes($id) {
+
+		return $this -> db -> get_where('usuarios', array('id' => $id)) -> row_array();
+	}
+
 }
